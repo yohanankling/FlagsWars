8dec4501ee5c908f945c03f9e9c2d998fb399322
yohanan kling <yohanankli@gmail.com>
2023-05-01 20:29:08 +0300

    Delete server directory

--- server/src/services/gameService.ts
+++
@@ -1,9 +0,0 @@
-import { firebaseDb } from '..';
-import { IGameManagerData } from 'common';
-
-export const updateGameData = async (gameId: string, gameData: IGameManagerData) => {
-  await firebaseDb.ref(`games`).child(gameId).update({
-    game_data: gameData,
-  });
-};
-
