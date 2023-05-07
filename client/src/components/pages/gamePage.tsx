61beb3c8301d8355269a318e4434e0b6e616ba19
yohanan <yohanankli@gmail.com>
2023-05-07 11:19:36 +0300

    online game page wip get vsname

--- client/src/components/pages/gamePage.tsx
+++ client/src/components/pages/gamePage.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { ref, onValue } from 'firebase/database';
 import { realTimeDb, auth } from '../../firebase/firebase';
 import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
 import '../../css/game.css';
 import {
   Cell,
@@ -14,19 +15,123 @@ import {
   team,
   GameManagerFactory,
   Child,
+  Death,
+  Devil,
+  Dwarf,
+  Flag,
+  Knight,
+  Mommy,
+  Ninja,
+  Odin,
+  Team,
+  Thor,
+  Troll,
+  Vampire,
+  Viking,
+  Wizard,
 } from 'common';
 import { move, setReady } from '../../services/onlineGameSerivce';
+import React from 'react';
+import authService from '../../services/authService';
+import { send } from '../../services/httpContext';
+const contactImg = require('../../icons/contact.png');
+const logoutImg = require('../../icons/logout.png');
+const profileImg = require('../../icons/profile.png');
+const homeImg = require('../../icons/home.png');
 
-const redPawnImage = require('../../assets/devil_blue.png');
-const bluePawnImage = require('../../assets/devil_red.png');
-const redKingImage = require('../../assets/death_red.png');
-const blueKingImage = require('../../assets/death_blue.png');
+const user = auth.currentUser;
+let name = "YOU";
+if (user){name = user.displayName;}
+const board = require('../../icons/board.png');
+
+
+const child_blue = require('../../assets/child_blue.png');
+const child_red = require('../../assets/child_red.png');
+const death_blue = require('../../assets/death_blue.png');
+const death_red = require('../../assets/death_red.png');
+const devil_blue = require('../../assets/devil_blue.png');
+const devil_red = require('../../assets/devil_red.png');
+const dwarf_blue = require('../../assets/dwarf_blue.png');
+const dwarf_red = require('../../assets/dwarf_red.png');
+const flag_blue = require('../../assets/flag_blue.png');
+const flag_red = require('../../assets/flag_red.png');
+const knight_blue = require('../../assets/knight_blue.png');
+const knight_red = require('../../assets/knight_red.png');
+const mommy_blue = require('../../assets/mommy_blue.png');
+const mommy_red = require('../../assets/mommy_red.png');
+const ninja_blue = require('../../assets/ninja_blue.png');
+const ninja_red = require('../../assets/ninja_red.png');
+const odin_blue = require('../../assets/odin_blue.png');
+const odin_red = require('../../assets/odin_red.png');
+const thor_blue = require('../../assets/thor_blue.png');
+const thor_red = require('../../assets/thor_red.png');
+const troll_blue = require('../../assets/troll_blue.png');
+const troll_red = require('../../assets/troll_red.png');
+const vampire_blue = require('../../assets/vampire_blue.png');
+const vampire_red = require('../../assets/vampire_red.png');
+const viking_blue = require('../../assets/viking_blue.png');
+const viking_red = require('../../assets/viking_red.png');
+const wizard_blue = require('../../assets/wizard_blue.png');
+const wizard_red = require('../../assets/wizard_red.png');
 const eyeImage = require('../../assets/eye.png');
 
-// this is an ugly hack to inject the images to the imported common project
 Child.getImage = function (entity) {
-  return entity.team === team.blue ? bluePawnImage : redPawnImage;
+  return entity.team === team.blue ? child_blue : child_red;
 };
+
+Death.getImage = function (entity) {
+  return entity.team === team.blue ? death_blue : death_red;
+};
+
+Devil.getImage = function (entity) {
+  return entity.team === team.blue ? devil_blue : devil_red;
+};
+
+Dwarf.getImage = function (entity) {
+  return entity.team === team.blue ? dwarf_blue : dwarf_red;
+};
+
+Flag.getImage = function (entity) {
+  return entity.team === team.blue ? flag_blue : flag_red;
+};
+
+Knight.getImage = function (entity) {
+  return entity.team === team.blue ? knight_blue : knight_red;
+};
+
+Mommy.getImage = function (entity) {
+  return entity.team === team.blue ? mommy_blue : mommy_red;
+};
+
+Ninja.getImage = function (entity) {
+  return entity.team === team.blue ? ninja_blue : ninja_red;
+};
+
+
+Odin.getImage = function (entity) {
+  return entity.team === team.blue ? odin_blue : odin_red;
+};
+
+Thor.getImage = function (entity) {
+  return entity.team === team.blue ? thor_blue : thor_red;
+};
+
+Troll.getImage = function (entity) {
+  return entity.team === team.blue ? troll_blue : troll_red;
+};
+
+Vampire.getImage = function (entity) {
+  return entity.team === team.blue ? vampire_blue : vampire_red;
+};
+
+Viking.getImage = function (entity) {
+  return entity.team === team.blue ? viking_blue : viking_red;
+};
+
+Wizard.getImage = function (entity) {
+  return entity.team === team.blue ? wizard_blue : wizard_red;
+};
+
 
 interface selectedEntity {
   x: number;
@@ -35,6 +140,8 @@ interface selectedEntity {
 }
 
 const GamePage = () => {
+  const navigate = useNavigate();
+  const [vsName, setVsName] = useState("");
   const { id } = useParams();
   const [gameDetails, setGameDetails] = useState<{
     player1: { id: string; team: team };
@@ -56,6 +163,25 @@ const GamePage = () => {
     }[]
   >([]);
 
+  const fetchDoc = async () => {
+    let fetchUid = user.uid;
+    if (gameDetails.player1.id === user.uid){
+      fetchUid = gameDetails.player2.id;
+    }
+    try {
+      const res = await send({ method: 'POST', route: '/getDoc', data: { uid: fetchUid } });
+      let doc = res.data;
+      // alert(fetchUid)
+      // setVsName(doc.name);
+      // setVsName("aaa");
+    } catch (error: any) {
+      console.error(error);}
+  };
+
+  useEffect(() => {
+    fetchDoc();
+  }, []);
+
   const listenForGame = () => {
     const starCountRef = ref(realTimeDb, 'games/' + id);
     onValue(starCountRef, (snapshot) => {
@@ -73,8 +199,14 @@ const GamePage = () => {
     }
 
     const currentTeamPiecesSetup = getTeam(currentTeam).piecesSetup;
-    const kingImage = currentTeam === team.red ? redKingImage : blueKingImage;
-
+    const dwarfImage = currentTeam === team.blue ? dwarf_blue : dwarf_red;
+    const flagImage = currentTeam === team.blue ? flag_blue : flag_red;
+    const devilImage = currentTeam === team.blue ? devil_blue : devil_red;
+    const deathImage = currentTeam === team.blue ? death_blue : death_red;
+    const wizardImage = currentTeam === team.blue ? wizard_blue : wizard_red;
+    const trollImage = currentTeam === team.blue ? troll_blue : troll_red;
+    const mommyImage = currentTeam === team.blue ? mommy_blue : mommy_red;
+    const ninjaImage = currentTeam === team.blue ? ninja_blue : ninja_red;
     let isFinished: boolean = true;
     for (const k in currentTeamPiecesSetup) {
       let key = k as entityType;
@@ -86,30 +218,137 @@ const GamePage = () => {
 
     return (
       <div>
-        <div>
-          <img
-            src={kingImage}
-            width={100}
-            height={100}
-            onClick={() => {
-              if (currentTeamPiecesSetup.child === 0) {
-                return;
-                //show error
-              }
-
-              const t = getTeam(currentTeam);
-              setSelectedEntity({ entity: new Child(t.team), x: -1, y: -1 });
-              const highlightBoard = new MarkerBoard();
-              for (let i = 0; i < 8; i++) {
-                if (!gameManager.board.getCell(i, t.FIRST_COLUMN).entity) {
-                  highlightBoard.setHighlight(i, t.FIRST_COLUMN);
-                }
-              }
-
-              setHighlightBoard(highlightBoard);
-            }}
-          />
-          {/*<p>Count: {getTeam(currentTeam).piecesSetup.king + getTeam(currentTeam).piecesSetup.dwarf}</p>*/}
+        <div className="icons">
+          <br/>
+          {currentTeamPiecesSetup.dwarf === 0 ? null :
+            <img className="image"
+                 src={dwarfImage}
+                 onClick={() => {
+                   const t = getTeam(currentTeam);
+                   setSelectedEntity({ entity: new Dwarf(t.team), x: -1, y: -1 });
+                   const highlightBoard = new MarkerBoard();
+                   for (let i = 0; i < 8; i++) {
+                     if (!gameManager.board.getCell(i, 7).entity) {
+                       highlightBoard.setHighlight(i, 7);
+                     }
+                   }
+                   setHighlightBoard(highlightBoard);
+                 }}
+            />
+          }
+          {currentTeamPiecesSetup.death === 0 ? null :
+            <img className="image"
+                 src={deathImage}
+                 onClick={() => {
+                   const t = getTeam(currentTeam);
+                   setSelectedEntity({ entity: new Death(t.team), x: -1, y: -1 });
+                   const highlightBoard = new MarkerBoard();
+                   for (let i = 0; i < 8; i++) {
+                     if (!gameManager.board.getCell(i, 7).entity) {
+                       highlightBoard.setHighlight(i, 7);
+                     }
+                   }
+                   setHighlightBoard(highlightBoard);
+                 }}
+            />
+          }
+          {currentTeamPiecesSetup.devil === 0 ? null :
+            <img className="image"
+                 src={devilImage}
+                 onClick={() => {
+                   const t = getTeam(currentTeam);
+                   setSelectedEntity({ entity: new Devil(t.team), x: -1, y: -1 });
+                   const highlightBoard = new MarkerBoard();
+                   for (let i = 0; i < 8; i++) {
+                     if (!gameManager.board.getCell(i, 7).entity) {
+                       highlightBoard.setHighlight(i, 7);
+                     }
+                   }
+                   setHighlightBoard(highlightBoard);
+                 }}
+            />
+          }
+          {currentTeamPiecesSetup.flag === 0 ? null :
+            <img className="image"
+                 src={flagImage}
+                 onClick={() => {
+                   const t = getTeam(currentTeam);
+                   setSelectedEntity({ entity: new Flag(t.team), x: -1, y: -1 });
+                   const highlightBoard = new MarkerBoard();
+                   for (let i = 0; i < 8; i++) {
+                     if (!gameManager.board.getCell(i, 7).entity) {
+                       highlightBoard.setHighlight(i, 7);
+                     }
+                   }
+                   setHighlightBoard(highlightBoard);
+                 }}
+            />
+          }
+          <br/>
+          {currentTeamPiecesSetup.mommy === 0 ? null :
+            <img className="image"
+                 src={mommyImage}
+                 onClick={() => {
+                   const t = getTeam(currentTeam);
+                   setSelectedEntity({ entity: new Mommy(t.team), x: -1, y: -1 });
+                   const highlightBoard = new MarkerBoard();
+                   for (let i = 0; i < 8; i++) {
+                     if (!gameManager.board.getCell(i, 7).entity) {
+                       highlightBoard.setHighlight(i, 7);
+                     }
+                   }
+                   setHighlightBoard(highlightBoard);
+                 }}
+            />
+          }
+          {currentTeamPiecesSetup.ninja === 0 ? null :
+            <img className="image"
+                 src={ninjaImage}
+                 onClick={() => {
+                   const t = getTeam(currentTeam);
+                   setSelectedEntity({ entity: new Ninja(t.team), x: -1, y: -1 });
+                   const highlightBoard = new MarkerBoard();
+                   for (let i = 0; i < 8; i++) {
+                     if (!gameManager.board.getCell(i, 7).entity) {
+                       highlightBoard.setHighlight(i, 7);
+                     }
+                   }
+                   setHighlightBoard(highlightBoard);
+                 }}
+            />
+          }
+          {currentTeamPiecesSetup.wizard === 0 ? null :
+            <img className="image"
+                 src={wizardImage}
+                 onClick={() => {
+                   const t = getTeam(currentTeam);
+                   setSelectedEntity({ entity: new Wizard(t.team), x: -1, y: -1 });
+                   const highlightBoard = new MarkerBoard();
+                   for (let i = 0; i < 8; i++) {
+                     if (!gameManager.board.getCell(i, 7).entity) {
+                       highlightBoard.setHighlight(i, 7);
+                     }
+                   }
+                   setHighlightBoard(highlightBoard);
+                 }}
+            />
+          }
+          {currentTeamPiecesSetup.troll === 0 ? null :
+            <img className="image"
+                 src={trollImage}
+                 onClick={() => {
+                   const t = getTeam(currentTeam);
+                   setSelectedEntity({ entity: new Troll(t.team), x: -1, y: -1 });
+                   const highlightBoard = new MarkerBoard();
+                   for (let i = 0; i < 8; i++) {
+                     if (!gameManager.board.getCell(i, 7).entity) {
+                       highlightBoard.setHighlight(i, 7);
+                     }
+                   }
+                   setHighlightBoard(highlightBoard);
+                 }}
+            />
+          }
         </div>
         {isFinished ? (
           <div>
@@ -213,24 +452,44 @@ const GamePage = () => {
   }, [initialLoad]);
 
   return (
+    <div className={"background"}>
+      <img className={"background"} src={board} alt={"background"}/>
+      <div className={"cover"}>
+        <>
+          <div className='navbar'>
+            <h4 className='name'>{name} vs {vsName}</h4>
+            <div className='navbarBtns'>
+              <button className='homeBtn'
+                      onClick={() => {
+                        navigate('/');
+                      }}>
+                <img src={homeImg} alt='Home' />
+              </button>
+              <button className='contactBtn'
+                      onClick={() => {
+                        navigate('/contact');
+                      }}>
+                <img src={contactImg} alt='Contact' />
+              </button>
+              <button className='profileBtn'
+                      onClick={() => {
+                        navigate('/profile');
+                      }}>
+                <img className='profileImg' src={profileImg} alt='Profile' />
+              </button>
+              <button className='logoutBtn'
+                      onClick={() => {
+                        authService.signOut();
+                      }}>
+                <img src={logoutImg} alt='Logout' />
+              </button>
+            </div>
+          </div>
+        </>
     <>
       {initialLoad ? (
-        <div>
-          <h1>Game Id: {id}</h1>
+        <div className='details'>
           <div>
-            <h3>Player1: {gameDetails.player1.id}</h3>
-            <p>Team: {team[gameDetails.player1.team]}</p>
-          </div>
-          <div>
-            <h3>Player2: {gameDetails.player2.id}</h3>
-            <p>Team: {team[gameDetails.player2.team]}</p>
-          </div>
-
-          <h4>Turn Count: {gameManager.turnCount}</h4>
-
-          <div>
-            {!gameManager.setupFinished ? <p>Place your pieces</p> : <p>Game started</p>}
-
             <div className="board">
               {gameManager.board.board?.map((row: Cell[], y: number) => (
                 <div key={y} className="row">
@@ -238,7 +497,11 @@ const GamePage = () => {
                     return (
                       <button
                         key={x}
-                        className={classNames('cell', `${color[highlightBoard.returnHighlightType(x, y)]}-highlight`)}
+                        className={classNames('cell', {
+                          [`${color[highlightBoard.returnHighlightType(x, y)]}-highlight`]: true,
+                          'light': (y + x) % 2 === 0,
+                          'dark': (y + x) % 2 === 1,
+                        })}
                         onClick={() => handleCellClick(cell)}
                       >
                         {renderPieceImage(cell?.entity as Entity)}
@@ -248,14 +511,22 @@ const GamePage = () => {
                 </div>
               ))}
             </div>
-
-            {renderGameSetup()}
+            <div className='messagesBox'>
+              <div>
+                {!gameManager.setupFinished ? <p className='message'>Place your pieces</p> : <p className='message'>Game started</p>}
+                <div className='icons'>
+                  {renderGameSetup()}
+                </div>
+              </div>
+              </div>
           </div>
         </div>
       ) : (
         <h2>Loading Game...</h2>
       )}
     </>
+    </div>
+    </div>
   );
 };
 
